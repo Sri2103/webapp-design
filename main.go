b786f857b4c2c04fbe580d947928856099dfd31b
Sri2103 <damasriharsha@gmail.com>
2022-01-17 14:48:27 -0500

    added a new router with MUX and handled the routing

--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-// create a server function to handle request and write resonse
+// using  newmux
+// create a server function to handle request and write response
 func HelloServer(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "hello World")
 }
 
 func main() {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", HelloServer)
+	//handle the server function with mux router
+
+	mux.HandleFunc("/", HelloServer)
+
+	log.Println("listening on Port :8080")
 
 	// address port to handle above server
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 
 }
